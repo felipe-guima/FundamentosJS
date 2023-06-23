d8e1590fd46400fd6ddf9cb7b014f61fb8b75a1a
Felipe Guimaraes <feelipe.guima@gmail.com>
2023-06-23 02:01:04 -0300

    adicionando constantes em tipo-number

--- tipo-number.js
+++ tipo-number.js
@@ -32,3 +32,9 @@ const Felipe = "Felipe"
 
 console.log(Felipe * primeiro);
 
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+
+console.log(operacao);
